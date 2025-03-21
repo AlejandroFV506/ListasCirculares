eec1d809c5fcadaef6038fdc4af6b9ad160f9d0d
AlejandroFV <aflores@estudiantec.cr>
2025-03-20 18:27:36 -0600

    Agregar archivos de proyecto.

---
+++ ListaDobleCircular/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDobleCircular
+{
+    internal class Node
+    {
+        public int Data {  get; set; }
+        public Node Next { get; set; }
+        public Node Previus { get; set; }
+
+        public Node(int value) 
+        {
+            this.Data = value;
+            this.Next = null;
+            this.Previus = null;
+        }
+    }
+}
---
+++ ListaDobleCircular/listCircular.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDobleCircular
+{
+    public class listCircular
+    {
+        private Node Tail = null; // cola de la lista
+        public int Size { get; private set; } // # elementos lista
+
+        public bool IsEmpty()
+        {  
+            return Tail == null;
+        }
+
+        public void addFirst(int newElement)
+        {
+            Node newNode = new Node(newElement);
+
+            if (IsEmpty())
+            {
+
+                Tail = newNode;
+                Tail.Next = Tail;
+                Tail.Previus = Tail;
+            }
+
+            else
+            {
+
+                newNode.Next = Tail.Next; // 
+                newNode.Previus = Tail;
+                Tail.Next.Previus = newNode;
+                Tail.Next = newNode;
+            }
+
+
+        }
+    }
+}
