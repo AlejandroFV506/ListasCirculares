66ab5927801b1a4da7c79ab48164f1c02b541360
ItsLean <lruiz@estudiantec.cr>
2025-03-20 19:51:01 -0600

    Commit Lista Circular Simple

---
+++ ListaCircularSimple/ListaCircularSimple.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaCircularSimple
+{
+    public class ListaCircular
+    {
+        private Nodo cabeza;
+        private int tamaño;
+
+        public ListaCircular()
+        {
+            cabeza = null;
+            tamaño = 0;
+        }
+
+        public void InsertarAlInicio(int valor)
+        {
+            Nodo nuevoNodo = new Nodo(valor);
+            if (cabeza == null)
+            {
+                nuevoNodo.Siguiente = nuevoNodo; // Apunta a sí mismo
+                cabeza = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.Siguiente = cabeza;
+                Nodo temp = cabeza;
+                while (temp.Siguiente != cabeza)
+                {
+                    temp = temp.Siguiente;
+                }
+                temp.Siguiente = nuevoNodo;
+                cabeza = nuevoNodo;
+            }
+            tamaño++;
+        }
+
+        public void InsertarAlFinal(int valor)
+        {
+            Nodo nuevoNodo = new Nodo(valor);
+            if (cabeza == null)
+            {
+                nuevoNodo.Siguiente = nuevoNodo; // Apunta a sí mismo
+                cabeza = nuevoNodo;
+            }
+            else
+            {
+                Nodo temp = cabeza;
+                while (temp.Siguiente != cabeza)
+                {
+                    temp = temp.Siguiente;
+                }
+                temp.Siguiente = nuevoNodo;
+                nuevoNodo.Siguiente = cabeza;
+            }
+            tamaño++;
+        }
+
+        public void InsertarEnPosicion(int valor, int indice)
+        {
+            if (indice < 0 || indice > tamaño)
+            {
+                throw new ArgumentOutOfRangeException("Índice fuera de rango");
+            }
+
+            if (indice == 0)
+            {
+                InsertarAlInicio(valor);
+            }
+            else if (indice == tamaño)
+            {
+                InsertarAlFinal(valor);
+            }
+            else
+            {
+                Nodo nuevoNodo = new Nodo(valor);
+                Nodo temp = cabeza;
+                for (int i = 0; i < indice - 1; i++)
+                {
+                    temp = temp.Siguiente;
+                }
+                nuevoNodo.Siguiente = temp.Siguiente;
+                temp.Siguiente = nuevoNodo;
+                tamaño++;
+            }
+        }
+
+        public void EliminarAlInicio()
+        {
+            if (cabeza == null)
+            {
+                throw new InvalidOperationException("La lista está vacía");
+            }
+
+            if (cabeza.Siguiente == cabeza)
+            {
+                cabeza = null;
+            }
+            else
+            {
+                Nodo temp = cabeza;
+                while (temp.Siguiente != cabeza)
+                {
+                    temp = temp.Siguiente;
+                }
+                temp.Siguiente = cabeza.Siguiente;
+                cabeza = cabeza.Siguiente;
+            }
+            tamaño--;
+        }
+
+        public void EliminarAlFinal()
+        {
+            if (cabeza == null)
+            {
+                throw new InvalidOperationException("La lista está vacía");
+            }
+
+            if (cabeza.Siguiente == cabeza)
+            {
+                cabeza = null;
+            }
+            else
+            {
+                Nodo temp = cabeza;
+                Nodo anterior = null;
+                while (temp.Siguiente != cabeza)
+                {
+                    anterior = temp;
+                    temp = temp.Siguiente;
+                }
+                anterior.Siguiente = cabeza;
+            }
+            tamaño--;
+        }
+
+        public void EliminarEnPosicion(int indice)
+        {
+            if (indice < 0 || indice >= tamaño)
+            {
+                throw new ArgumentOutOfRangeException("Índice fuera de rango");
+            }
+
+            if (indice == 0)
+            {
+                EliminarAlInicio();
+            }
+            else if (indice == tamaño - 1)
+            {
+                EliminarAlFinal();
+            }
+            else
+            {
+                Nodo temp = cabeza;
+                for (int i = 0; i < indice - 1; i++)
+                {
+                    temp = temp.Siguiente;
+                }
+                temp.Siguiente = temp.Siguiente.Siguiente;
+                tamaño--;
+            }
+        }
+
+        public int ObtenerTamaño()
+        {
+            return tamaño;
+        }
+
+        public override string ToString()
+        {
+            if (cabeza == null)
+            {
+                return "";
+            }
+
+            StringBuilder sb = new StringBuilder();
+            Nodo temp = cabeza;
+            do
+            {
+                sb.Append(temp.Valor);
+                if (temp.Siguiente != cabeza)
+                {
+                    sb.Append(", ");
+                }
+                temp = temp.Siguiente;
+            } while (temp != cabeza);
+
+            return sb.ToString();
+        }
+    }
+}
+
--- ListaCircularSimple/Program.cs
+++ ListaCircularSimple/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ListaCircularSimple;
+using System.Data.Common;
+class program
+{
+    static void Main(string[] args)
+    {
+        ListaCircular lista = new ListaCircular();
+
+        lista.InsertarAlFinal(1);
+        Console.WriteLine("Se agrego al final el numero 1: " + lista);
+        lista.InsertarAlFinal(2);
+        Console.WriteLine("Se agrego al final el numero 2: " + lista);
+        lista.InsertarAlFinal(3);
+        Console.WriteLine("Se agrego al final el numero 3: " + lista);
+        lista.InsertarAlInicio(0);
+        Console.WriteLine("se agrego al inicio el numero 0: " + lista);
+        lista.InsertarAlInicio(7);
+        Console.WriteLine("se agrego al inicio el numero 7: " + lista);
+        lista.InsertarEnPosicion(4, 2);
+        Console.WriteLine("se agrego el numero 4 en la posicion 2 de la lista: " + lista);
+        Console.WriteLine("La lista resultante es: " + lista.ToString()); // Output: 7, 0, 4, 1, 2, 3
+        int tamaño = lista.ObtenerTamaño(); // Output: 6
+        Console.WriteLine("El tamaño de la lista es de: "+ tamaño);
+        lista.EliminarAlInicio();
+        Console.WriteLine("se elimino el primer elemento de la lista: " + lista);
+        lista.EliminarAlFinal();
+        Console.WriteLine("se elimino el ultimo elemento de la lista: " + lista);
+        lista.EliminarEnPosicion(1);
+        Console.WriteLine("se elimino el elemento en la posicion 1 de la lista: " + lista);
+        Console.WriteLine("La lista resultante es: " + lista.ToString()); // Output: 0, 1, 2
+        tamaño = lista.ObtenerTamaño();
+        Console.WriteLine("El tamaño de la lista es de: "+ tamaño); // Output: 3   
+    }
+}
+
+
